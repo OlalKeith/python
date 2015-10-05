1c959d3801b8138c7b71249761faae5a180a2942
OlalKeith <jkeneth.jk@gmail.com>
2015-10-05 12:33:48 +0300

    modelforms

--- django_project/first_app/home/admin.py
+++ django_project/first_app/home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Student
+from forms import StudentForm
 # Register your models here.
 
+class StudentAdmin(admin.ModelAdmin):
+	form = StudentForm
 
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
