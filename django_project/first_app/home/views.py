e9e15853a66c5494e4589508543b7154a5cbc427
OlalKeith <jkeneth.jk@gmail.com>
2015-10-06 12:48:56 +0300

    overidingform

--- django_project/first_app/home/views.py
+++ django_project/first_app/home/views.py
@@ -3,10 +3,26 @@ from .forms import StudentForm
 
 # Create your views here.
 def index(request):
-	form = StudentForm()
-	print(request.POST)
+	form = StudentForm(request.POST or None)
+
 	context = {
-		"hello_message": "Hello Moringa",
+		"hello_message": "Register new Student",
 		"form":form
 	}
+
+	
+	if form. is_valid():
+
+		instance=form.save(commit=False)
+		full_name = form.cleaned_data.get('full_name')
+		if full_name == "Olal":
+			full_name = "Developer"
+		instance.full_name = full_name
+		instance.save()
+
+		context = {
+		"hello_message":"Student Saved"
+		}
+
 	return render(request,'index.html', context)
+
