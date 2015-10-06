8f2ae1fca1cbc553ad1f4ec7be7210bdfafc0c35
OlalKeith <jkeneth.jk@gmail.com>
2015-10-06 10:13:45 +0300

    formview

--- django_project/first_app/home/views.py
+++ django_project/first_app/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
+	form = StudentForm()
 	context = {
-		"hello_message": "Hello Moringa"
+		"hello_message": "Hello Moringa",
+		"form":form
 	}
 	return render(request,'index.html', context)
