cdb69c3607730df9a4fd9fc80500f0b2cd3b219f
OlalKeith <jkeneth.jk@gmail.com>
2015-10-06 11:56:40 +0300

    posttoform

--- django_project/first_app/home/views.py
+++ django_project/first_app/home/views.py
@@ -4,6 +4,7 @@ from .forms import StudentForm
 # Create your views here.
 def index(request):
 	form = StudentForm()
+	print(request.POST)
 	context = {
 		"hello_message": "Hello Moringa",
 		"form":form
