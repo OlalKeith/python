e44d1fec8d0d4e7ba21b4efe1dfa7ba546af3fc9
OlalKeith <jkeneth.jk@gmail.com>
2015-10-07 15:48:22 +0300

    sendemail

--- django_project/first_app/first_app/settings.py
+++ django_project/first_app/first_app/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST ='smtp.mandrillapp.com'
+EMAIL_HOST_USER = 'jkeneth.jk@gmail.com'
+EMAIL_HOST_PASSWORD = '9qonjrKgNF6IpMDprfAMog'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
--- django_project/first_app/home/views.py
+++ django_project/first_app/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import StudentForm
 from .forms import FeedbackForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
@@ -31,8 +32,14 @@ def feedback(request):
 
 	form = FeedbackForm(request.POST or None)
 	if form. is_valid():
-		for key, value in form.cleaned_data.items():
-			print(key, value)
+		from_email = form.cleaned_data.get('email')
+		full_name = form.cleaned_data.get('full_name')
+		message = form.cleaned_data.get('message')
+		prepared_message = "You have feedback from {} saying '{}'".format(full_name, message)
+		send_mail('New feedback given', prepared_message, from_email,
+		['jkeneth.jk@gmail.com'], fail_silently=False)
+		#for key, value in form.cleaned_data.items():
+			#print(key, value)
 	context = {
 	"form":form
 	}
